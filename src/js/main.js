231af24bde13db7324fd7106e4892603d84a6c22
ValterMonteiro <valtinms@gmail.com>
2022-11-25 22:36:41 -0400

    ✨ Funcionalidade: Consultar  e exibir dados da API

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,40 @@
 import './nav.js'
+
+const formConsultaPerfil = document.querySelector('#consultaPerfil');
+const inputNome = formConsultaPerfil.nome;
+const divDados = document.querySelector('#dados');
+
+formConsultaPerfil.addEventListener('submit', function(event){
+
+ 
+event.preventDefault();
+//ativaLoader(true);
+consultaPerfil(inputNome.value);
+
+});
+
+async function consultaPerfil(nome){
+
+ //Passando o enderço da api -> https://api.github.com/users/maqueise
+ let response = await fetch(`https://api.github.com/users/${nome}`);
+
+ let dadosPerfil = await response.json();
+
+ divDados.innerHTML = `
+ <img src="${dadosPerfil.avatar_url}" alt="">
+ <p>${dadosPerfil.name}</p>
+ <a href="${dadosPerfil.html_url}">Perfil no Github</a>
+ `
+ 
+ //ativaLoader(false)
+};
+
+function ativaLoader(ativo) {
+    if (ativo) {
+      divDados.setAttribute('aria-busy', 'true')
+      divDados.textContent = 'Aguarde! localizando perfil … '
+    } else {
+      divDados.removeAttribute('aria-busy')
+      divDados.textContent = 'Consultar'
+    }
+  }
